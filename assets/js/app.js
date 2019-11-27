01da941e99ca4990edd4685fda8265aada55ac56
Nicolas <nicolas.deux11@orange.fr>
2019-11-27 07:30:15 +0100

    invoqué webpack encore

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+
+require('../css/app.css');
+require ('ckeditor4');
+
+const $ = require('jquery');
+
+// $(function () {
+//     $('textarea.ckeditor').ckeditor();
+// });
+
+//CKEDITOR.replace( 'personage_speech_content' );
+
+
